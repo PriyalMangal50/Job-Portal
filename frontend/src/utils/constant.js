aa1e14c116edfd152f18a438d78fee4fca773bde
PriyalMangal50 <145255577+PriyalMangal50@users.noreply.github.com>
2025-01-28 19:15:39 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,5 @@
-export const USER_API_END_POINT="https://job-portal-6wfw.onrender.com/api/v1/user";
+export const USER_API_END_POINT="https://job-portal-6wfw.onrender.com/v1/user";
 export const JOB_API_END_POINT="https://job-portal-6wfw.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT="https://job-portal-6wfw.onrender.com/api/v1/application";
-<<<<<<< HEAD
 export const COMPANY_API_END_POINT="https://job-portal-6wfw.onrender.com/api/v1/company";
-=======
 export const COMPANY_API_END_POINT="https://job-portal-6wfw.onrender.com/api/v1/company";
->>>>>>> fc7f55ef39d30c5e0f65a12945d4a1d5829829b0
