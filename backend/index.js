b86b9180ed5834789353296c2686a7a24b1d3117
PriyalMangal50 <thepriyalmangal@gmail.com>
2025-01-29 00:21:56 +0530

    Resolved merge conflict in index.js

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,17 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 import path from "path";
-<<<<<<< HEAD
+
 import dotenv from 'dotenv';
 dotenv.config({ path: './backend/.env' });
 
 console.log('MONGO_URI:', process.env.MONGO_URI); // Specifically check if MONGO_URI is loaded
 
-=======
 
 // Load environment variables from the backend .env file
 dotenv.config({ path: './backend/.env' });
 
 // Connect to MongoDB
->>>>>>> 3e8a60bf2d10086d8aacb28a6c662f3e5c1d6105
 connectDB();
 
 const app = express();
@@ -31,7 +29,7 @@ const _dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-<<<<<<< HEAD
+
 const corsOptions = {
     origin: [
         'https://mern-job-portal-67oy.onrender.com', // Deployed frontend
@@ -41,21 +39,6 @@ const corsOptions = {
     credentials: true, // Allow cookies
 };
 app.use(cors(corsOptions));
-
-
-=======
-
-// CORS Configuration
-const corsOptions = {
-  origin: [
-    'https://job-portal-gold-ten.vercel.app',  // Allow your deployed frontend (production)
-  ],
-  credentials: true,
-};
-app.use(cors(corsOptions));
-
-// Backend server port configuration
->>>>>>> 3e8a60bf2d10086d8aacb28a6c662f3e5c1d6105
 const PORT = process.env.PORT || 5000;
 
 // API Routes
@@ -68,12 +51,10 @@ app.use("/api/v1/application", applicationRoute);
 // Make sure the 'frontend/dist' or 'frontend/build' folder contains the built files after running `npm run build` in the frontend
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
-<<<<<<< HEAD
 app.listen(PORT,()=>{
    
     console.log(`Server running at port ${PORT}`);
 })
-=======
 // Catch-all route to serve the frontend app
 app.get('*', (req, resp) => {
   resp.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
@@ -83,4 +64,3 @@ app.get('*', (req, resp) => {
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
 });
->>>>>>> 3e8a60bf2d10086d8aacb28a6c662f3e5c1d6105
